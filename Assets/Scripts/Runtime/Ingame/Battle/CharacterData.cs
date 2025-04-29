9b1f86ca7f314ffbdfe1b1619c3013983ff4da89
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-30 02:09:01 +0900

    [update]データに体力と名前の入力を追加

--- Assets/Scripts/Runtime/Ingame/Battle/CharacterData.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/CharacterData.cs
@@ -1,9 +1,16 @@
+using SymphonyFrameWork.Config;
 using UnityEngine;
 
 namespace BeatKeeper.Runtime.Ingame.Character
 {
     public class CharacterData : ScriptableObject
     {
+        public string Name => _name;
+        [SerializeField, Tooltip("名前")] string _name;
+        
+        public float MaxHealth => _maxHealth;
+        [SerializeField, Tooltip("最大体力値")] private float _maxHealth;
+        
         
     }
 }
