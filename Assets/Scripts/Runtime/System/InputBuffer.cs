a084188ffa9f0f220d140e98f736cfe68ffab11a
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-30 04:40:21 +0900

    [update]とりま攻撃と移動だけ入力を実装

--- Assets/Scripts/Runtime/System/InputBuffer.cs
+++ Assets/Scripts/Runtime/System/InputBuffer.cs
@@ -12,12 +12,21 @@ namespace BeatKeeper
     {
         private PlayerInput _playerInput;
 
+        public InputAction Move => _move;
+        private InputAction _move;
+        
+        public InputAction Attack => _attack;
+        private InputAction _attack;
+
         private void Awake()
         {
             _playerInput = GetComponent<PlayerInput>();
             if (_playerInput)
             {
                 _playerInput.notificationBehavior = PlayerNotifications.InvokeCSharpEvents;
+
+                _move = _playerInput.actions["Move"];
+                _attack = _playerInput.actions["Attack"];
             }
             else
             {
