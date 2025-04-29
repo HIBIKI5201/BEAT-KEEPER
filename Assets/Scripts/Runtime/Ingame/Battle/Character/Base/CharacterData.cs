941560431a5de9cbeed8f7528a27943ad684ff4e
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-30 02:42:10 +0900

    [update]クリエイトメニューに追加

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Base/CharacterData.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Base/CharacterData.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 namespace BeatKeeper.Runtime.Ingame.Character
 {
+    [CreateAssetMenu(menuName = CharacterDataDirectory + "NormalData")]
     public class CharacterData : ScriptableObject
     {
+        public const string CharacterDataDirectory = "BeatKeeper/CharacterData/";
+        
         public string Name => _name;
-        [SerializeField, Tooltip("名前")] string _name;
+        [SerializeField, Tooltip("名前")] private string _name;
         
         public float MaxHealth => _maxHealth;
         [SerializeField, Tooltip("最大体力値")] private float _maxHealth;
