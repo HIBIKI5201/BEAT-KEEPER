8d623a050c367b37bbfeb775c2a960c6ce6d2039
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-30 02:26:04 +0900

    [update]キャラクターのベースクラスを実装

---
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Base/CharacterManagerB.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace BeatKeeper.Runtime.Ingame.Character
+{
+    /// <summary>
+    ///     キャラクター管理クラスのベース
+    /// </summary>
+    /// <typeparam name="TDataType">キャラクターのデータ</typeparam>
+    public abstract class CharacterManagerB<TDataType> : MonoBehaviour where TDataType : CharacterData
+    {
+        [FormerlySerializedAs("data")] [SerializeField]
+        protected TDataType _data;
+        protected CharacterHealthSystem _healthSystem;
+
+        protected virtual void Awake()
+        {
+            _healthSystem = new CharacterHealthSystem(_data);
+            Debug.Log("初期化完了");
+        }
+    }
+}
