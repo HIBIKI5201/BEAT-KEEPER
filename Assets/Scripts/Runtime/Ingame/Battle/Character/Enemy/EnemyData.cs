286ec4162e47ec4e1b77dd35c551af6ff6dfb805
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-30 22:32:28 +0900

    [update]譜面のデータをbool配列に変更

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Enemy/EnemyData.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Enemy/EnemyData.cs
@@ -8,8 +8,8 @@ namespace BeatKeeper.Runtime.Ingame.Character
     [CreateAssetMenu(fileName = "EnemyData", menuName = CharacterData.CHARACTER_DATA_DIRECTORY + "EnemyData")]
     public class EnemyData : CharacterData
     {
-        public float Beat => _beat;
+        public bool[] Beat => _beat;
         [SerializeField, Tooltip("ビートの拍子")]
-        private float _beat = 1;
+        private bool[] _beat = new bool[32];
     }
 }
