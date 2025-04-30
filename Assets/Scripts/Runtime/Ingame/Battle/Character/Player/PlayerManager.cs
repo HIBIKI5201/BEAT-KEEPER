eb23dacd5debc62fa1cf9e8c15f376067144185f
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-30 21:58:34 +0900

    [update]攻撃入力を受け付けられるように

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Player/PlayerManager.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Player/PlayerManager.cs
@@ -1,15 +1,41 @@
+using System;
+using SymphonyFrameWork.System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace BeatKeeper.Runtime.Ingame.Character
 {
     /// <summary>
     ///     プレイヤーのマネージャークラス
     /// </summary>
-    public class PlayerManager : CharacterManagerB<CharacterData>
+    public class PlayerManager : CharacterManagerB<CharacterData>, IDisposable
     {
+        private InputBuffer _inputBuffer;
+
+        private int _comboCount;
+        
         protected override void Awake()
         {
             base.Awake();
+            
+            tag = TagsEnum.Player.ToString();
+        }
+
+        private void Start()
+        {
+            _inputBuffer = ServiceLocator.GetInstance<InputBuffer>();
+
+            _inputBuffer.Attack.started += OnAttack;
+        }
+
+        public void Dispose()
+        {
+            _inputBuffer.Attack.started -= OnAttack;
+        }
+
+        private void OnAttack(InputAction.CallbackContext context)
+        {
+            Debug.Log($"{_data.Name} is attacking");
         }
     }
 }
