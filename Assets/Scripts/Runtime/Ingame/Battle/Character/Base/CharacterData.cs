a58602879422016182763b76281facd667c69b5a
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-30 21:42:31 +0900

    [update]攻撃力パラメータを追加

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Base/CharacterData.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Base/CharacterData.cs
@@ -13,8 +13,13 @@ namespace BeatKeeper.Runtime.Ingame.Character
         
         public string Name => _name;
         [SerializeField, Tooltip("名前")] private string _name = "empty name";
+
+        public float AttackPower => _attackPower;
+        [SerializeField, Tooltip("攻撃力")] private float _attackPower = 10;
         
+        # region 未使用パラメータ
         public float MaxHealth => _maxHealth;
-        [SerializeField, Tooltip("最大体力値")] private float _maxHealth = 100;
+        [Tooltip("最大体力値")] private float _maxHealth = 100;
+        #endregion
     }
 }
