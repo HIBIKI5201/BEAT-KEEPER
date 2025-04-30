786833ba0b17071c9dba8f4a583009c9ac971a6c
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-30 23:19:26 +0900

    [update]ノックバックを実行

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Enemy/TestEnemyManager.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Enemy/TestEnemyManager.cs
@@ -5,9 +5,21 @@ namespace BeatKeeper
 {
     public class TestEnemyManager : CharacterManagerB<EnemyData>
     {
+        private bool _isKnockback;
+        
         protected override void Awake()
         {
             base.Awake();
+        }
+
+        public override async void HitAttack(float damage)
+        {
+            base.HitAttack(damage);
+            
+            //ノックバック
+            _isKnockback = true;
+            await Awaitable.WaitForSecondsAsync(1, destroyCancellationToken);
+            _isKnockback = false;
         }
     }
 }
