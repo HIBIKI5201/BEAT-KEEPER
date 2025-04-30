1678737e9b2a837a7a5434dbb2b5ff06f99221aa
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-30 22:47:31 +0900

    [update]コンボ時間のデータを追加

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Player/PlayerData.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Player/PlayerData.cs
@@ -7,13 +7,18 @@ namespace BeatKeeper.Runtime.Ingame.Character
     {
         [Space]
         
-        [SerializeField] private float _firstAttackPower;
+        [SerializeField] private float _firstAttackPower = 100;
         public float FirstAttackPower => _firstAttackPower;
         
-        [SerializeField] private float _secondAttackPower;
+        [SerializeField] private float _secondAttackPower = 100;
         public float SecondAttackPower => _secondAttackPower;
         
-        [SerializeField] private float _thirdAttackPower;
+        [SerializeField] private float _thirdAttackPower = 100;
         public float ThirdAttackPower => _thirdAttackPower;
+
+        [Space] 
+        
+        public float ComboResetTime;
+        [SerializeField, Tooltip("コンボ維持時間")] private float _comboRisetTime = 3;
     }
 }
