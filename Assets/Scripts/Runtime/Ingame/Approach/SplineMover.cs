63c574a73cfaca3303ebf2ed14d7f74e44f73c45
tsutsumikiriku <tsutsumikiriku@gmail.com>
2025-05-05 15:22:56 +0900

    [fix]コメントと命名を修正

--- Assets/Scripts/Runtime/Ingame/Approach/SplineMover.cs
+++ Assets/Scripts/Runtime/Ingame/Approach/SplineMover.cs
@@ -7,10 +7,13 @@ using SymphonyFrameWork.Utility;
 
 namespace BeatKeeper.Runtime.Ingame.Approach
 {
+    /// <summary>
+    /// SplineMoverは、SplineContainerを使用してTransformを移動させるクラスです。
+    /// </summary>
     public class SplineMover : IPausable
     {
         SplineContainer _splineContainer;
-        Transform _transform;
+        Transform _playerTransform;
         float _skipTime;
         int _progress = 0;
         Tween _moveTween;
@@ -18,12 +21,12 @@ namespace BeatKeeper.Runtime.Ingame.Approach
         /// SplineMoverのコンストラクタ
         /// /// </summary>
         /// <param name="splineContainer">SplineContainer</param>
-        /// <param name="transform">Transform</param>
+        /// <param name="transform">プレイヤーのTransform</param>
         /// <param name="skipTime">スキップする時間。MoveToNextのtimeより短くある必要があります。</param>
         public SplineMover(SplineContainer splineContainer,Transform transform,float skipTime = 0.1f)
         {
             _splineContainer = splineContainer;
-            _transform = transform;
+            _playerTransform = transform;
             _skipTime = skipTime;
             IPausable.RegisterPauseManager(this);
         }
@@ -49,25 +52,32 @@ namespace BeatKeeper.Runtime.Ingame.Approach
                 Debug.Log("全てのSplineを移動しました。");
                 return;
             }
+            //Spline上の進捗を保持する変数の初期化
             float progressOnSpline = 0f;
-            _moveTween?.Kill();
+            //Splineの進捗をTweenで移動させる
             _moveTween = DOTween.To(() => progressOnSpline, x => progressOnSpline = x, 1f, time)
                 .OnUpdate(() =>
                 {
-                    _transform.forward = _splineContainer.Splines[_progress].EvaluateTangent(progressOnSpline);
-                    _transform.position = _splineContainer.Splines[_progress].EvaluatePosition(progressOnSpline);
+                    //playerの前方をSplineの接線に合わせる
+                    _playerTransform.forward = _splineContainer.Splines[_progress].EvaluateTangent(progressOnSpline);
+                    //playerの位置をSpline上の進捗に合わせる
+                    _playerTransform.position = _splineContainer.Splines[_progress].EvaluatePosition(progressOnSpline);
                 }).OnComplete(() =>
                 {
+                    //完了したらSplineの進捗を1つ進める
                     _progress++;
                 });
         }
         async UniTask SkipToNext(float time)
         {
+            //Splianeの数を超えた場合は何もしない
             if(_progress >= _splineContainer.Splines.Count)return;
-            _moveTween = _transform.DOMove(_splineContainer.Splines[_progress].EvaluatePosition(0),time);
+            //Tweenを使用して移動する
+            _moveTween = _playerTransform.DOMove(_splineContainer.Splines[_progress].EvaluatePosition(0),time);
+            //Tweenが完了するまで待機する
             await _moveTween.AsyncWaitForCompletion();
         }
-        //インターフェイスの実装
+        //Pauseインターフェイスの実装
         public void Pause()
         {
             if (_moveTween.IsActive())
