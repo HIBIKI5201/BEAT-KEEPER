4cce175dfd0f707e1a8d27bd795c38c726abdf43
HIBIKI_5201 <takuto5201@gmail.com>
2025-05-07 12:18:32 +0900

    [add]プレイヤー用のアニメマネージャーを追加

---
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Player/PlayerAnimeManager.cs
@@ -0,0 +1,10 @@
+using BeatKeeper.Runtime.Ingame.Character;
+using UnityEngine;
+
+namespace BeatKeeper.Runtime.Ingame.Battle.Character.Player
+{
+    public class PlayerAnimeManager : CharacterAnimeManagerB
+    {
+        public PlayerAnimeManager(Animator animator) : base(animator) { }
+    }
+}
