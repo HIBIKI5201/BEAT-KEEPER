7c36a528eaaf9d746e09997660ed40f8c0719272
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-11 12:17:26 +0900

    [add] リズム共鳴カウントのUIを作成

--- Assets/Scripts/Runtime/Ingame/UI/Battle/UIElement_FlowZoneMeter.cs
+++ Assets/Scripts/Runtime/Ingame/UI/Battle/UIElement_FlowZoneMeter.cs
@@ -1,4 +1,7 @@
+using BeatKeeper.Runtime.Ingame.Character;
+using R3;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace BeatKeeper
 {
@@ -7,6 +10,41 @@ namespace BeatKeeper
     /// </summary>
     public class UIElement_FlowZoneMeter : MonoBehaviour
     {
+        [SerializeField] private PlayerManager _playerManager;
+        [SerializeField] private Color _defaultColor = Color.black;
+        [SerializeField] private Color _resonanceColor = Color.yellow;
+        [SerializeField] private Image[] _icons;
+        private CompositeDisposable _disposable = new CompositeDisposable();
         
+        private void Start()
+        {
+            _playerManager.FlowZoneSystem.ResonanceCount.Subscribe(IconColorChanged).AddTo(_disposable);
+            _playerManager.FlowZoneSystem.IsFlowZone.Subscribe(value => { if(!value) AllReset(); }).AddTo(_disposable);
+            AllReset();
+        }
+
+        /// <summary>
+        /// リズム共鳴回数に合わせてアイコンの色を変更する
+        /// </summary>
+        private void IconColorChanged(int count)
+        {
+            _icons[count].color = _resonanceColor;
+        }
+
+        /// <summary>
+        /// アイコンを全て暗い色に設定する
+        /// </summary>
+        private void AllReset()
+        {
+            foreach (var icon in _icons)
+            {
+                icon.color = _defaultColor;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _disposable?.Dispose();
+        }
     }
 }
