aeb2eb58955ab16f9e02e7d836bd2dbd192c3601
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-17 22:32:13 +0900

    [add] プレイヤー用のマズルフラッシュを管理するクラスを作成

---
+++ Assets/Scripts/Runtime/Ingame/Battle/Bullet/PlayerMuzzleFlachHandler.cs
@@ -0,0 +1,28 @@
+using BeatKeeper.Runtime.Ingame.Character;
+using SymphonyFrameWork.System;
+using UnityEngine;
+
+namespace BeatKeeper.Runtime.Ingame
+{
+    /// <summary>
+    /// プレイヤーのマズルフラッシュエフェクトの再生タイミングを管理するクラス
+    /// </summary>
+    public class PlayerMuzzleFlachHandler : MonoBehaviour
+    {
+        [SerializeField] private MuzzleFlashController _muzzleFlash = new MuzzleFlashController();
+        private PlayerManager _playerManager;
+        
+        private void Start()
+        {
+            _playerManager = ServiceLocator.GetInstance<PlayerManager>();
+            
+            // プレイヤーが攻撃したタイミングでマズルフラッシュのエフェクトが再生されるようにする
+            _playerManager.OnComboAttack += _muzzleFlash.Fire;
+        }
+
+        private void OnDestroy()
+        {
+            _playerManager.OnComboAttack -= _muzzleFlash.Fire;
+        }
+    }
+}
