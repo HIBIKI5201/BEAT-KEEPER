c524dfa710aea0e0499fea2993fe51e4bd223732
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-17 22:32:53 +0900

    [fix] マズルフラッシュコントローラーの不要な部分を削除

--- Assets/Scripts/Runtime/Ingame/Battle/Bullet/MuzzleFlashController.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Bullet/MuzzleFlashController.cs
@@ -11,21 +11,14 @@ namespace BeatKeeper.Runtime.Ingame
     {
         [SerializeField] private Transform _muzzle; // 銃口の位置
         [SerializeField] private ParticleSystem _muzzleFlash; // マズルフラッシュのエフェクト
-
-        public MuzzleFlashController(Transform muzzle, ParticleSystem muzzleFlash)
-        {
-            _muzzle = muzzle;
-            _muzzleFlash = muzzleFlash;
-        }
         
         /// <summary>
         /// 弾を発射する
         /// </summary>
         public void Fire()
         {
-            if (_muzzleFlash != null)
+            if (_muzzleFlash != null || _muzzle != null)
             {
-                // エフェクトが設定されていたらマズルフラッシュを再生
                 _muzzleFlash.transform.position = _muzzle.position; // 座標を銃口に合わせる
                 _muzzleFlash.Play();
             }
