990ce4e966b671c1b13dffeb5063f193f5e9f9a0
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-17 22:18:03 +0900

    [fix] 旧IAttackableをIHitableに名前変更

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Base/IHitable.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Base/IHitable.cs
@@ -1,9 +1,11 @@
-using UnityEngine;
+using System;
 
 namespace BeatKeeper.Runtime.Ingame.Character
 {
-    public interface IAttackable
+    public interface IHitable
     {
         public void HitAttack(float damage);
+        
+        public Action OnHitAttack { get; set; }
     }
 }
