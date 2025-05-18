5a48532a4394073b9e9fbd2ccaf2c7f55e8dcd1a
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-05-18 11:29:23 +0900

    [update] HitEffectController仮実装

--- Assets/Scripts/Runtime/Ingame/Battle/Bullet/HitEffectController.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Bullet/HitEffectController.cs
@@ -1,3 +1,4 @@
+using BeatKeeper.Runtime.Ingame.Character;
 using UnityEngine;
 
 namespace BeatKeeper.Runtime.Ingame
@@ -8,6 +9,33 @@ namespace BeatKeeper.Runtime.Ingame
     public class HitEffectController : MonoBehaviour
     {
         [SerializeField] private ParticleSystem _hitEffect; // 被弾時のエフェクト
-        
+        private IHitable _target; // 攻撃を受ける対象（自身）
+
+        private void Start()
+        {
+            if (!TryGetComponent(out _target))
+            {
+                Debug.LogAssertion($"{gameObject.name} に {typeof(IHitable)}が設定されていません");
+                return;
+            }
+            
+            _target.OnHitAttack += PlayHitEffect;
+        }
+
+        /// <summary>
+        /// ヒット時にエフェクトを再生する
+        /// </summary>
+        private void PlayHitEffect()
+        {
+            if(!_hitEffect) return; // nullなら以降の処理は行わない
+            
+            transform.position = _hitEffect.transform.position; // 座標を設定する
+            _hitEffect.Play();
+        }
+
+        private void OnDestroy()
+        {
+            _target.OnHitAttack -= PlayHitEffect;
+        }
     }
 }
