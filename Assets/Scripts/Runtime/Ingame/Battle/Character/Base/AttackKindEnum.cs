755390c03001f59990c0510504320ad215ba4765
HIBIKI_5201 <takuto5201@gmail.com>
2025-05-21 19:52:48 +0900

    [update]攻撃種類Enumをフラグ型に変更

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Base/AttackKindEnum.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Base/AttackKindEnum.cs
@@ -1,10 +1,13 @@
+using System;
+
 namespace BeatKeeper.Runtime.Ingame.Battle
 {
-    public enum AttackKindEnum
+    [Flags]
+    public enum AttackKindEnum : int
     {
         None = 0,
-        Normal = 1,
-        Charge = 2,
-        Super = 3
+        Normal = 1 << 0,
+        Charge = 1 << 1,
+        Super = 1 << 2
     }
 }
