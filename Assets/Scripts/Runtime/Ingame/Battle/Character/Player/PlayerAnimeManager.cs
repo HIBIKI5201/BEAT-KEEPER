4c76dc412455c3ff9464f08a1a38c5d66034664f
HIBIKI_5201 <takuto5201@gmail.com>
2025-05-21 20:29:13 +0900

    [update]アニメーションマネージャーにパラメータ変更メソッドを追加

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Player/PlayerAnimeManager.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Player/PlayerAnimeManager.cs
@@ -1,10 +1,24 @@
 using BeatKeeper.Runtime.Ingame.Character;
+using TMPro;
 using UnityEngine;
 
 namespace BeatKeeper.Runtime.Ingame.Character
 {
     public class PlayerAnimeManager : CharacterAnimeManagerB
     {
+        private readonly int _moveX = Animator.StringToHash("MoveX");
+        private readonly int _moveZ = Animator.StringToHash("MoveZ");
+        
+        private readonly int _avoid = Animator.StringToHash("Avoid");
+        
         public PlayerAnimeManager(Animator animator) : base(animator) { }
+        
+        public void Avoid() => _animator.SetTrigger(_avoid);
+
+        public void MoveVector(Vector2 direction)
+        {
+            _animator.SetFloat(_moveX, direction.x);
+            _animator.SetFloat(_moveZ, direction.y);
+        }
     }
 }
