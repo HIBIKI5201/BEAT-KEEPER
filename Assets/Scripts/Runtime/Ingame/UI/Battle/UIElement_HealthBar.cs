833b470ebb701f3fb99b3cafd4a34683fba2f35e
HIBIKI_5201 <takuto5201@gmail.com>
2025-05-27 23:45:41 +0900

    [update]ヘルスバーの更新を行う

--- Assets/Scripts/Runtime/Ingame/UI/Battle/UIElement_HealthBar.cs
+++ Assets/Scripts/Runtime/Ingame/UI/Battle/UIElement_HealthBar.cs
@@ -1,3 +1,9 @@
+using System.Linq;
+using BeatKeeper.Runtime.Ingame.Battle;
+using BeatKeeper.Runtime.Ingame.Character;
+using SymphonyFrameWork.Debugger;
+using SymphonyFrameWork.System;
+using SymphonyFrameWork.Utility;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,12 +12,38 @@ namespace BeatKeeper.Runtime.Ingame.UI
     public class UIElement_HealthBar : MonoBehaviour
     {
         [SerializeField] private Image _bar;
+        private CharacterHealthSystem _enemyHealthSystem;
 
         private void Start()
         {
             if (!_bar) Debug.LogWarning("bar is null");
+
+            ServiceLocator.RegisterAfterLocate<BattleSceneManager>(RegisterEnemyEvent);
+
+            async void RegisterEnemyEvent()
+            {
+                var manager = ServiceLocator.GetInstance<BattleSceneManager>();
+                if (manager)
+                {
+                    var enemy = manager.EnemyAdmin.Enemies.First();
+                    
+                    await SymphonyTask.WaitUntil(() => enemy.HealthSystem != null);
+
+                    _enemyHealthSystem = enemy.HealthSystem;
+                    _enemyHealthSystem.OnHealthChanged += OnEnemyHealthChange;
+                }
+                else Debug.LogWarning("manager is null");
+            }
         }
 
-        public void SetBarAmount(float amount) => _bar.fillAmount = amount;
+        private void OnEnemyHealthChange(float health)
+        {
+            SetBarAmount(health / _enemyHealthSystem.MaxHealth);
+        }
+
+        private void SetBarAmount(float amount)
+        {
+            if (_bar) _bar.fillAmount = amount;
+        }
     }
 }
