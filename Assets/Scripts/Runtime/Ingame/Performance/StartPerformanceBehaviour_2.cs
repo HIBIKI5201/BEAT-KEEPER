ba207776aed9984e3c98707a5cffb938f4a43af0
HIBIKI_5201 <takuto5201@gmail.com>
2025-05-28 04:44:12 +0900

    [add]スタートパフォーマンスのPlayableBehaviourを追加

---
+++ Assets/Scripts/Runtime/Ingame/Performance/StartPerformanceBehaviour_2.cs
@@ -0,0 +1,26 @@
+using BeatKeeper.Runtime.Ingame.Stsge;
+using BeatKeeper.Runtime.Ingame.System;
+using SymphonyFrameWork.System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace BeatKeeper
+{
+    public class StartPerformanceBehaviour_2 : PlayableBehaviour
+    {
+        public override void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            base.OnBehaviourPlay(playable, info);
+            
+            var cameraManager = ServiceLocator.GetInstance<StageManager>().CameraManager;
+            cameraManager.ChangeCamera(0);
+            var uiManager = ServiceLocator.GetInstance<InGameUIManager>();
+            uiManager.HideEncounterText();
+            uiManager.BattleStart();
+            var bgmChanger = ServiceLocator.GetInstance<BGMChanger>();
+            bgmChanger.ChangeBGM("Battle1");
+            
+            Debug.Log("StartPerformanceBehaviour_2");
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Ingame/Performance/StartPerformanceBehaviour_3.cs
@@ -0,0 +1,19 @@
+using SymphonyFrameWork.System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace BeatKeeper
+{
+    public class StartPerformanceBehaviour_3 : PlayableBehaviour
+    {
+        public override void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            base.OnBehaviourPlay(playable, info);
+
+            var phaseManager = ServiceLocator.GetInstance<PhaseManager>();
+            phaseManager.NextPhase();
+            
+            Debug.Log("StartPerformanceBehaviour_3");
+        }
+    }
+}
