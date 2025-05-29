71c1c6bc86704ce5ad9e998fdc8da56a4e739a6a
HIBIKI_5201 <takuto5201@gmail.com>
2025-05-30 00:24:36 +0900

    [add]バトルバフのデータクラスを追加

---
+++ Assets/Scripts/Runtime/Ingame/System/Battle/BattleBuffTimelineData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace BeatKeeper.Runtime.Ingame.Battle
+{
+    [CreateAssetMenu(fileName = nameof(BattleBuffTimelineData), menuName = "BeatKeeper/" + nameof(BattleBuffTimelineData))]
+    public class BattleBuffTimelineData : ScriptableObject
+    {
+        
+    }
+}
