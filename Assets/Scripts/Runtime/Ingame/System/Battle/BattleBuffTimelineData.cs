59301febdbf698c3bbea5dbaf13c63fe8082cfc0
HIBIKI_5201 <takuto5201@gmail.com>
2025-05-31 11:13:36 +0900

    [update]バフのデータクラスを追加

--- Assets/Scripts/Runtime/Ingame/System/Battle/BattleBuffTimelineData.cs
+++ Assets/Scripts/Runtime/Ingame/System/Battle/BattleBuffTimelineData.cs
@@ -1,10 +1,23 @@
+using System;
 using UnityEngine;
 
 namespace BeatKeeper.Runtime.Ingame.Battle
 {
-    [CreateAssetMenu(fileName = nameof(BattleBuffTimelineData), menuName = "BeatKeeper/" + nameof(BattleBuffTimelineData))]
+    [CreateAssetMenu(fileName = nameof(BattleBuffTimelineData),
+        menuName = "BeatKeeper/" + nameof(BattleBuffTimelineData))]
     public class BattleBuffTimelineData : ScriptableObject
     {
-        
+        [Serializable]
+        public struct BuffData
+        {
+            [SerializeField, Tooltip("バフが入る拍数"), Min(0)] private int _timing;
+            public int Timing => _timing;
+
+            [SerializeField] private float _value;
+            public float Value => _value;
+        }
+
+        [SerializeField] private BuffData[] _data;
+        public BuffData[] Data => _data;
     }
 }
