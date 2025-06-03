b5381f016a07162425645cee946bd44a067ec715
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-03 18:07:17 +0900

    [update]バトルシーンの初期化をイベント関数に

--- Assets/Scripts/Runtime/Ingame/System/Battle/BattleSceneManager.cs
+++ Assets/Scripts/Runtime/Ingame/System/Battle/BattleSceneManager.cs
@@ -1,4 +1,3 @@
-using BeatKeeper.Runtime.Ingame.Stage;
 using BeatKeeper.Runtime.Ingame.System;
 using UnityEngine;
 
@@ -6,22 +5,8 @@ namespace BeatKeeper.Runtime.Ingame.Battle
 {
     public class BattleSceneManager : SceneManagerB
     {
-        public StageEnemyAdmin EnemyAdmin => _enemyAdmin;
-        private StageEnemyAdmin _enemyAdmin;
-        
         [SerializeField]
-        private Transform _enemiesParent;
-
-        private void Awake()
-        {
-            if (_enemiesParent)
-            {
-                _enemyAdmin = new (_enemiesParent);
-            }
-            else
-            {
-                Debug.LogWarning("EnemiesParent is null");
-            }
-        }
+        private StageEnemyAdmin _enemyAdmin;
+        public StageEnemyAdmin EnemyAdmin => _enemyAdmin;
     }
 }
