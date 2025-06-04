14dadca31a08e80c6a1b6f86ca8d72cd41aca83f
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-04 23:37:22 +0900

    [fix]クリアフェーズマネージャーのCameraChangeもコメントアウト

--- Assets/Scripts/Runtime/Ingame/Clear/ClearPhaseManager.cs
+++ Assets/Scripts/Runtime/Ingame/Clear/ClearPhaseManager.cs
@@ -51,7 +51,7 @@ namespace BeatKeeper
                 // リザルト表示、NPCにフォーカス。NPCが褒めてくれる演出
                 ShowBattleResult();
                 _uiManager.BattleEnd();
-                _cameraManager.ChangeCamera(1);
+                //_cameraManager.ChangeCamera(1);
             }
             else if (_count == 5)
             {
@@ -64,7 +64,7 @@ namespace BeatKeeper
                 _objects[2].gameObject.SetActive(true); 
                 _encounterText.ShowEncounterText(2);
                 _objects[2].transform.DOMove(_positions[0], 4f); // NPCを追いかけている状態
-                _cameraManager.ChangeCamera(1); // カメラを向ける
+                //_cameraManager.ChangeCamera(); // カメラを向ける
             }
             else if (_count == 13)
             {
@@ -73,7 +73,7 @@ namespace BeatKeeper
                 _encounterText.HideEncounterText();
                 _objects[1].transform.DOMove(_positions[1], 4f); // 敵が戦闘位置まで移動
                 _objects[3].transform.LookAt(_objects[1].transform); // プレイヤーを次の敵の方に向かせる
-                _cameraManager.ChangeCamera(0);
+                //_cameraManager.ChangeCamera(0);
             }
             else if (_count == 17)
             {
