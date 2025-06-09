3c79064a9d4ef115a978fae0a1216b1d263264b8
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-10 01:35:29 +0900

    [add]フィニッシャーシークエンス2を追加

---
+++ Assets/Scripts/Runtime/Ingame/Sequence/FinisherSequence/FinisherSequenceBehaviour_2.cs
@@ -0,0 +1,27 @@
+﻿using BeatKeeper.Runtime.Ingame.Battle;
+using BeatKeeper.Runtime.Ingame.Stsge;
+using SymphonyFrameWork.System;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+namespace BeatKeeper.Runtime.Ingame.Sequence
+{
+    public class FinisherSequenceBehaviour_2 : PlayableBehaviour
+    {
+        public override void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            var enemy = ServiceLocator.GetInstance<BattleSceneManager>()
+                .EnemyAdmin.GetActiveEnemy();
+
+            if (enemy)
+            {
+                var remainHealth = enemy.HealthSystem.Health;
+                enemy.HitAttack(new(remainHealth));
+
+                var scoreManager = ServiceLocator.GetInstance<ScoreManager>();
+                scoreManager.AddScore(2000);
+            }
+        }
+    }
+}
