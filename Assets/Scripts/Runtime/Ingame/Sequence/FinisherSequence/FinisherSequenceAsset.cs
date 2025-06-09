02bbbdec614c2c68ca74f58e670d12cd1e46ef1b
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-10 01:40:45 +0900

    [update]フィニッシャー2をアサインできるように

--- Assets/Scripts/Runtime/Ingame/Sequence/FinisherSequence/FinisherSequenceAsset.cs
+++ Assets/Scripts/Runtime/Ingame/Sequence/FinisherSequence/FinisherSequenceAsset.cs
@@ -5,13 +5,25 @@ namespace BeatKeeper.Runtime.Ingame.Sequence
 {
     public class FinisherSequenceAsset : PlayableAsset
     {
+        [SerializeField, Range(1, 2)] private int _behaviour;
         public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
         {
-            var playable1 = ScriptPlayable<FinisherSequenceBehaviour_1>.Create(graph);
-            var behaviour1 = playable1.GetBehaviour();
-            behaviour1.OnCreate(owner);
+            switch (_behaviour)
+            {
+                case 1:
+                    var playable1 = ScriptPlayable<FinisherSequenceBehaviour_1>.Create(graph);
+                    var behaviour1 = playable1.GetBehaviour();
+                    behaviour1.OnCreate(owner);
 
-            return playable1;
+                    return playable1;
+
+                case 2:
+                    var playable2 = ScriptPlayable<FinisherSequenceBehaviour_2>.Create(graph);
+
+                    return playable2;
+            }
+
+            return Playable.Null;
         }
     }
 }
