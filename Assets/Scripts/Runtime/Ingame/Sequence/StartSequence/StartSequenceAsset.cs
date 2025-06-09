49ebe2a6b0feb69b92ef757592dd7307af99da84
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-09 20:26:02 +0900

    [fix]Enumから数値パラメータに変更

--- Assets/Scripts/Runtime/Ingame/Sequence/StartSequence/StartSequenceAsset.cs
+++ Assets/Scripts/Runtime/Ingame/Sequence/StartSequence/StartSequenceAsset.cs
@@ -7,36 +7,29 @@ namespace BeatKeeper.Runtime.Ingame.Sequence
     [Serializable]
     public class StartSequenceAsset : PlayableAsset
     {
-        private enum BehaviourKind
-        {
-            Behaviour1,
-            Behaviour2,
-            Behaviour3,
-        }
-        
-        [SerializeField] private BehaviourKind _behaviourKind;
-        
+        [SerializeField, Range(1, 3)] private int _behaviourKind;
+
         override public Playable CreatePlayable(PlayableGraph graph, GameObject owner)
         {
-            switch(_behaviourKind)
+            switch (_behaviourKind)
             {
-                case BehaviourKind.Behaviour1:
+                case 1:
                     var playable1 = ScriptPlayable<StartSequenceBehaviour_1>.Create(graph);
                     var behaviour1 = playable1.GetBehaviour();
-
                     behaviour1.OnCreate(owner);
+
                     return playable1;
 
-                case BehaviourKind.Behaviour2:
+                case 2:
                     var playable2 = ScriptPlayable<StartSequenceBehaviour_2>.Create(graph);
                     var behaviour2 = playable2.GetBehaviour();
-
                     behaviour2.OnCreate(owner);
+
                     return playable2;
 
-                    case BehaviourKind.Behaviour3:
+                case 3:
                     var playable3 = ScriptPlayable<StartSequenceBehaviour_3>.Create(graph);
-                    
+
                     return playable3;
             }
 
