5db87f3dce36ffb0ea69296e9dc76473116ab3da
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-10 23:48:43 +0900

    [add]シークエンスのアセットを追加

---
+++ Assets/Scripts/Runtime/Ingame/Sequence/BreakMovieSequence/BreakMovieSequenceAsset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace BeatKeeper.Runtime.Ingame.Sequence
+{
+    public class BreakMovieSequenceAsset : PlayableAsset
+    {
+        [SerializeField, Range(1, 1)] private int _behaviour;
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            switch (_behaviour)
+            {
+                case 1:
+                    var playable = ScriptPlayable<BreakMovieSequenceBehaviour_1>.Create(graph);
+                    return playable;
+            }
+            return Playable.Null;
+        }
+    }
+}
