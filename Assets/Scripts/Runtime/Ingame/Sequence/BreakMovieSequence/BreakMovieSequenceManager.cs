0e280cc2f76b38c79bfcedc4e49617cd5e55bff3
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-10 23:31:32 +0900

    [add]ブレイクムービーを追加

---
+++ Assets/Scripts/Runtime/Ingame/Sequence/BreakMovieSequence/BreakMovieSequenceManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace BeatKeeper
+{
+    public class BreakMovieSequenceManager : MonoBehaviour
+    {
+        private PlayableDirector _playableDirector;
+
+        private void Awake()
+        {
+            _playableDirector = GetComponent<PlayableDirector>();
+            if (_playableDirector == null)
+            {
+                Debug.LogWarning("PlayableDirector component is missing on FinisherSequence.");
+            }
+        }
+    }
+}
