4d7111931fb88f7aced024f1086271c495203ca0
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-10 23:35:41 +0900

    [update]フィニッシャー終了時のイベントを登録

--- Assets/Scripts/Runtime/Ingame/Sequence/BreakMovieSequence/BreakMovieSequenceManager.cs
+++ Assets/Scripts/Runtime/Ingame/Sequence/BreakMovieSequence/BreakMovieSequenceManager.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.Playables;
 
-namespace BeatKeeper
+namespace BeatKeeper.Runtime.Ingame.Sequence
 {
+    [RequireComponent(typeof(PlayableDirector))]
     public class BreakMovieSequenceManager : MonoBehaviour
     {
         private PlayableDirector _playableDirector;
@@ -10,10 +11,26 @@ namespace BeatKeeper
         private void Awake()
         {
             _playableDirector = GetComponent<PlayableDirector>();
-            if (_playableDirector == null)
+            if (!_playableDirector)
             {
                 Debug.LogWarning("PlayableDirector component is missing on FinisherSequence.");
             }
+        }
+
+        private void Start()
+        {
+            var finisher = transform.parent.GetComponentInChildren<FinisherSequenceManager>();
+            if (finisher)
+            {
+                finisher.OnFinisherSequenceEnd += OnFinisherSequenceEnd;
+            }
+        }
+
+        private void OnFinisherSequenceEnd()
+        {
+            if (!_playableDirector) return;
+
+            _playableDirector.Play();
         }
     }
 }
