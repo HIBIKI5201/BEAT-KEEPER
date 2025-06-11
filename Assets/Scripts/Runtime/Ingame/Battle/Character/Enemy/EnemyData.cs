66f7642d32900887afc610748573483ec2806d7d
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-11 18:30:22 +0900

    [update]プロパティをコード規則に合わせて修正

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Enemy/EnemyData.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Enemy/EnemyData.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using BeatKeeper.Runtime.Ingame.Battle;
 using BeatKeeper.Runtime.Ingame.System;
 using UnityEngine;
@@ -13,15 +13,14 @@ namespace BeatKeeper.Runtime.Ingame.Character
     public class EnemyData : CharacterData
     {
         public float MaxHealth => _maxHealth;
-        [SerializeField, Tooltip("最大体力値")] private float _maxHealth = 100;
-
-        [SerializeField, Tooltip("フィニッシャーの閾値"), Range(0, 100)] private float _finisherThreshold = 10;
         public float FinisherThreshold => _finisherThreshold;
-
-        [SerializeField, Tooltip("ノックバック時間")] private float _nockbackTime = 1;
+        public ChartData ChartData => _chartData;
         public float NockbackTime => _nockbackTime;
 
+        [SerializeField, Tooltip("最大体力値")] private float _maxHealth = 100;
+        [SerializeField, Tooltip("フィニッシャーの閾値"), Range(0, 100)] 
+        private float _finisherThreshold = 10;
+        [SerializeField, Tooltip("ノックバック時間")] private float _nockbackTime = 1;
         [SerializeField, Tooltip("譜面データ")] private ChartData _chartData;
-        public ChartData ChartData => _chartData;
     }
 }
