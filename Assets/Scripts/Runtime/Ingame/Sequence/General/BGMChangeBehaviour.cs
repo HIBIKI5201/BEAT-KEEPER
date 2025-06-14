357494c0467c4e6a5ad0a322b888637d3e916a8e
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-15 00:52:30 +0900

    [add]BGMChangeBehaviourを追加

---
+++ Assets/Scripts/Runtime/Ingame/Sequence/General/BGMChangeBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using BeatKeeper.Runtime.Ingame.System;
+using SymphonyFrameWork.System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace BeatKeeper
+{
+    public class BGMChangeBehaviour : PlayableBehaviour
+    {
+        private string _bgmName = string.Empty;
+
+        public void SetBGMName(string name) => _bgmName = name;
+
+        public override void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            var bgmManager = ServiceLocator.GetInstance<BGMManager>();
+            if (bgmManager)
+            {
+                bgmManager.ChangeBGM(_bgmName);
+            }
+        }
+    }
+}
