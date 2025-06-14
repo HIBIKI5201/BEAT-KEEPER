4e2f95075af26f6bc71fb9f15f2eaf68dc4d9290
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-15 00:52:39 +0900

    BGMChangeのアセットを追加

---
+++ Assets/Scripts/Runtime/Ingame/Sequence/General/BGMChangeAsset.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace BeatKeeper
+{
+    public class BGMChangeAsset : PlayableAsset
+    {
+        [SerializeField]
+        private string _bgmName = string.Empty;
+
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            var playable = ScriptPlayable<BGMChangeBehaviour>.Create(graph);
+            var behaviour = playable.GetBehaviour();
+            behaviour.SetBGMName(_bgmName);
+
+            return playable;
+        }
+    }
+}
