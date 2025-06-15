91dfcdaf5d63efb3635645eb79b536ff985c4f59
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-16 02:44:00 +0900

    [update]データを格納できるように

--- Assets/Scripts/Runtime/Ingame/UI/Battle/Indicator/RingIndicatorData.cs
+++ Assets/Scripts/Runtime/Ingame/UI/Battle/Indicator/RingIndicatorData.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using BeatKeeper.Runtime.Ingame.Battle;
+using System;
+using UnityEngine;
 
 namespace BeatKeeper.Runtime.Ingame.UI
 {
@@ -8,6 +10,25 @@ namespace BeatKeeper.Runtime.Ingame.UI
     [CreateAssetMenu(fileName = nameof(RingIndicatorData), menuName = "BeatKeeper/UI/RingIndicatorData")]
     public class RingIndicatorData : ScriptableObject
     {
+        public RingData[] RingDatas => _ringDatas;
 
+        [SerializeField, Tooltip("リングのデータ群")]
+        private RingData[] _ringDatas;
+    }
+
+    [Serializable]
+    public class RingData
+    {
+        public ChartKindEnum AttackKind => _attackKind;
+        public GameObject RingPrefab => _ringPrefab;
+        public int DefaultCapacity => _defaultCapacity;
+
+        [SerializeField] private ChartKindEnum _attackKind;
+
+        [SerializeField] private GameObject _ringPrefab;
+
+        [SerializeField]
+        [Tooltip("リングの事前用意数（ある程度の同時出現数を入力）")]
+        private int _defaultCapacity = 3;
     }
 }
