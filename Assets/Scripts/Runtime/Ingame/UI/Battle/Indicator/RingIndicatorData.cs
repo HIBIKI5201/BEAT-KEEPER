4758c0dccdd78e54cf9fc9e79369659684976320
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-16 02:31:39 +0900

    [add]リングインジケーターのデータベースクラスを追加

---
+++ Assets/Scripts/Runtime/Ingame/UI/Battle/Indicator/RingIndicatorData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace BeatKeeper.Runtime.Ingame.UI
+{
+    /// <summary>
+    ///     リングインジケーターのデータベース
+    /// </summary>
+    [CreateAssetMenu(fileName = nameof(RingIndicatorData), menuName = "BeatKeeper/UI/RingIndicatorData")]
+    public class RingIndicatorData : ScriptableObject
+    {
+
+    }
+}
