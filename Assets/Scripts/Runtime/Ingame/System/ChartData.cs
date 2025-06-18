dc650e8428c490b78be13d5fd1234d0d085633c0
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-19 01:14:24 +0900

    [update]譜面データを初期化できるように

--- Assets/Scripts/Runtime/Ingame/System/ChartData.cs
+++ Assets/Scripts/Runtime/Ingame/System/ChartData.cs
@@ -7,12 +7,17 @@ namespace BeatKeeper.Runtime.Ingame.System
     [CreateAssetMenu(fileName = "ChartData", menuName = "BeatKeeper/ChartData", order = 1)]
     public class ChartData : ScriptableObject
     {
-        private const int ChartLength = 32;
+        private const int ChartLength = 64;
 
         private void Awake()
         {
             if (_chart.Length != ChartLength)
                 Debug.LogWarning("譜面データの長さが不適切です。");
+        }
+
+        private void Reset()
+        {
+            _chart = new ChartDataElement[ChartLength];
         }
 
         public ChartDataElement[] Chart => _chart;
