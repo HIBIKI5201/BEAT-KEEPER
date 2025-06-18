061464986a699fc3b6fe6ecd1466d515ee643bcd
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-06-19 01:22:58 +0900

    [fix] Enumの更新

--- Assets/Scripts/Runtime/Ingame/System/MusicEngine/BGMEnum.cs
+++ Assets/Scripts/Runtime/Ingame/System/MusicEngine/BGMEnum.cs
@@ -5,9 +5,19 @@ namespace BeatKeeper
     /// </summary>
     public enum BGMEnum
     {
-        Approach,
-        Battle1,
-        Battle2,
-        Battle3,
+        /// <summary>
+        /// フェーズ1に突入するとき
+        /// </summary>
+        Phase1_Intro,
+        
+        /// <summary>
+        /// フェーズ2に突入するとき
+        /// </summary>
+        Phase2_Intro,
+        
+        /// <summary>
+        /// フェーズ3に突入するとき
+        /// </summary>
+        Phase3_Intro,
     }
 }
