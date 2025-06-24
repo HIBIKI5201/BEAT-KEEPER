f9b07c89344cd3ffdc4a096b23833249a4063882
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-25 01:08:38 +0900

    [update]ビジブル状態を保存するパラメータを追加

--- Assets/Scripts/Runtime/Ingame/System/ChartData.cs
+++ Assets/Scripts/Runtime/Ingame/System/ChartData.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 namespace BeatKeeper.Runtime.Ingame.System
 {
     [CreateAssetMenu(fileName = "ChartData", menuName = "BeatKeeper/ChartData", order = 1)]
-    public class ChartData : ScriptableObject
+    public partial class ChartData : ScriptableObject
     {
-        private const int ChartLength = 64;
+        private const int CHART_LENGTH = 64;
 
         private void Awake()
         {
-            if (_chart.Length != ChartLength)
+            if (_chart.Length != CHART_LENGTH)
                 Debug.LogWarning($"{name}の譜面データの長さが不適切です。");
         }
 
         private void Reset()
         {
-            _chart = new ChartDataElement[ChartLength];
+            _chart = new ChartDataElement[CHART_LENGTH];
+#if UNITY_EDITOR
+            _visible = new bool[CHART_LENGTH];
+#endif
         }
 
         public ChartDataElement[] Chart => _chart;
         [SerializeField, Tooltip("ビートの拍子")]
-        private ChartDataElement[] _chart = new ChartDataElement[ChartLength];
+        private ChartDataElement[] _chart = new ChartDataElement[CHART_LENGTH];
 
         /// <summary>
         /// 指定したインデックスが攻撃かどうかを判定する
@@ -51,5 +54,11 @@ namespace BeatKeeper.Runtime.Ingame.System
             public ChartKindEnum AttackKind;
             public Vector2 Position;
         }
+
+        //エディタ用データ
+#if UNITY_EDITOR
+        [SerializeField, HideInInspector]
+        private bool[] _visible = new bool[CHART_LENGTH];
+#endif
     }
 }
