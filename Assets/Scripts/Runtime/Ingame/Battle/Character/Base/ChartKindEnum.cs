b9c750a27fa4a3ae246f6b2cf8c46c7a3869fb1e
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-02 20:02:56 +0900

    [update]コメントを追加

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Base/ChartKindEnum.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Base/ChartKindEnum.cs
@@ -6,10 +6,10 @@ namespace BeatKeeper.Runtime.Ingame.Battle
     public enum ChartKindEnum : int
     {
         None = 0,
-
+        //プレイヤーアクション
         Attack = 1 << 0,
         Skill = 1 << 1,
-
+        //エネミーアクション
         Normal = 1 << 2,
         Charge = 1 << 3,
         Super = 1 << 4,
--- Assets/Scripts/Runtime/Ingame/Battle/Character/Base/IHitable.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Base/IHitable.cs
@@ -1,10 +1,17 @@
-using BeatKeeper.Runtime.Ingame.Battle;
+﻿using BeatKeeper.Runtime.Ingame.Battle;
 using System;
 
 namespace BeatKeeper.Runtime.Ingame.Character
 {
+    /// <summary>
+    ///   攻撃を受けることができるインターフェース
+    /// </summary>
     public interface IHitable
     {
+        /// <summary>
+        /// /     攻撃を受けた時の処理
+        /// </summary>
+        /// <param name="damage"></param>
         public void HitAttack(AttackData damage);
 
         public Action<int> OnHitAttack { get; set; }
