cdc42323fd4ff26d468e922aada9de241a433975
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-02 19:55:59 +0900

    [fix]不要な名前空間を削除

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Base/CharacterData.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Base/CharacterData.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using SymphonyFrameWork.Config;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace BeatKeeper.Runtime.Ingame.Character
 {
@@ -11,7 +9,7 @@ namespace BeatKeeper.Runtime.Ingame.Character
     public class CharacterData : ScriptableObject
     {
         protected const string CHARACTER_DATA_DIRECTORY = "BeatKeeper/CharacterData/";
-        
+
         public string Name => _name;
 
         [SerializeField, Tooltip("名前")] private string _name = "empty name";
