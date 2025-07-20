bffcece9e4ba4bd276e34f7ecf8cb6db00a5af81
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-20 11:44:55 +0900

    [fix]一旦画像のスコアがアニメーションするように実装

--- Assets/Scripts/Runtime/Ingame/UI/Battle/UIElement_ScoreText.cs
+++ Assets/Scripts/Runtime/Ingame/UI/Battle/UIElement_ScoreText.cs
@@ -8,23 +8,41 @@ namespace BeatKeeper
     /// <summary>
     /// スコアテキストを更新するためのクラス
     /// </summary>
-    [RequireComponent(typeof(Text))]
     public class UIElement_ScoreText : MonoBehaviour
     {
         [SerializeField] private ScoreManager _scoreManager;
         [SerializeField] private float _animationDuration = 0.5f;
-        [SerializeField] private string _scoreFormat = "000000000000";
+        [SerializeField] private string _scoreFormat = "00000000";
 
-        private Text _text;
+        [SerializeField] private Sprite[] _numberSprites = new Sprite[10]; // 数字の画像素材
+        [SerializeField] private Image[] _scoreImages = new Image[8];
+        
         private int _currentDisplayScore = 0; // 現在表示されているスコア
         private Tweener _scoreTween;
         private CompositeDisposable _disposable = new CompositeDisposable();
 
         private void Start()
         {
-            _text = GetComponent<Text>();
+            if (_scoreManager == null)
+            {
+                Debug.LogError("ScoreManager がアサインされていません");
+                return;
+            }
+            
+            // スコア増加のリアクティブプロパティを購読して値の変動時にスコアテキスト更新処理が実行されるようにする
             _scoreManager.ScoreProp.Subscribe(UpdateScore).AddTo(_disposable);
-            _text.text = _scoreFormat; // 初期スコア表示
+            
+            for (int index = 0; index < _scoreImages.Length; index++)
+            {
+                if (_scoreImages[index] == null)
+                {
+                    // アサインされていなかった場合は、子オブジェクトから自動で取得する
+                    _scoreImages[index] = transform.GetChild(index).GetComponent<Image>();
+                }
+            }
+            
+            // 初期スコア表示
+            DisplayScore(0);
         }
 
         /// <summary>
@@ -40,14 +58,18 @@ namespace BeatKeeper
 
             // DOTweenを使用して現在の表示スコアから目標スコアまでアニメーション
             _scoreTween = DOTween.To(
-                () => _currentDisplayScore,
-                value =>
+                    () => _currentDisplayScore,
+                    value =>
+                    {
+                        _currentDisplayScore = value;
+                        DisplayScore(value);
+                    },
+                    targetScore, _animationDuration).SetEase(Ease.OutQuad)
+                .OnComplete(() => 
                 {
-                    _currentDisplayScore = value;
-                    _text.text = value.ToString(_scoreFormat);
-                },
-                targetScore, _animationDuration).SetEase(Ease.OutQuad)
-                .OnComplete(() => _currentDisplayScore = targetScore);  // アニメーション完了時に現在のスコアを確実に目標値に設定
+                    _currentDisplayScore = targetScore;
+                    DisplayScore(targetScore); // アニメーション完了時に確実に目標値を表示
+                });
         }
 
         /// <summary>
@@ -55,6 +77,23 @@ namespace BeatKeeper
         /// </summary>
         public void SavePreBattleScore() => _scoreManager.SavePreBattleScore();
 
+        /// <summary>
+        /// スコアを画像で表示する
+        /// </summary>
+        private void DisplayScore(int score)
+        {
+            string scoreString = score.ToString(_scoreFormat);
+            
+            for (int i = 0; i < _scoreImages.Length; i++)
+            {
+                if (i < scoreString.Length)
+                {
+                    int digit = int.Parse(scoreString[i].ToString());
+                    _scoreImages[i].sprite = _numberSprites[digit];
+                }
+            }
+        }
+        
         private void OnDestroy()
         {
             if (_scoreTween != null && _scoreTween.IsActive())
