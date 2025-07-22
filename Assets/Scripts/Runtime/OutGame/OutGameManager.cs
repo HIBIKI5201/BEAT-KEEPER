59e27a0144bf1907b7f2a140d50e873bd9e4ccff
reitaghchi <ren.len2426@gmail.com>
2025-07-22 14:08:12 +0900

    [add]シーン読み込み用のメソッド作成

--- Assets/Scripts/Runtime/OutGame/OutGameManager.cs
+++ Assets/Scripts/Runtime/OutGame/OutGameManager.cs
@@ -7,6 +7,35 @@ namespace BeatKeeper
 {
     public class OutGameManager : MonoBehaviour
     {
+        private const string OutGameSceneName = "OutGame";
+        private const string InGameSceneName = "InGame";
+        private const string StageSceneName = "Stage";
+        public async void Awake()
+        {
+            await LoadSceneAsync(StageSceneName);
+            //await LoadInGameSceneAsync();
+        }
 
+        /// <summary>
+        /// インゲームシーンを読み込むメソッド
+        /// </summary>
+        /// <returns></returns>
+        async Task LoadInGameSceneAsync()
+        {
+            await SceneLoader.UnloadScene(OutGameSceneName);
+            await SceneLoader.LoadScene(InGameSceneName);
+            SceneLoader.SetActiveScene(InGameSceneName);
+        }
+
+        /// <summary>
+        /// 文字列で指定したシーンを非同期で読み込むメソッド
+        /// </summary>
+        /// <param name="sceneName"></param>
+        /// <returns></returns>
+        async Task LoadSceneAsync(string sceneName)
+        {
+            // シーンの非同期読み込み
+            await SceneLoader.LoadScene(sceneName);
+        }
     }
 }
