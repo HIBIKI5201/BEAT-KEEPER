a1d69bb57b4a30065ea4aabb52973ac2a6a9244d
reitaghchi <ren.len2426@gmail.com>
2025-07-22 14:02:54 +0900

    [add]OutGameManager作成開始

---
+++ Assets/Scripts/Runtime/OutGame/OutGameManager.cs
@@ -0,0 +1,12 @@
+﻿using Cysharp.Threading.Tasks;
+using SymphonyFrameWork.System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BeatKeeper
+{
+    public class OutGameManager : MonoBehaviour
+    {
+
+    }
+}
