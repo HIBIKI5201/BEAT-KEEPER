8755d28276ccb2ec6d9e54c260dcb6f03e555097
reitaghchi <ren.len2426@gmail.com>
2025-07-23 12:21:10 +0900

    [update]TooltIp追加

--- Assets/Scripts/Runtime/OutGame/OutGameUIManager.cs
+++ Assets/Scripts/Runtime/OutGame/OutGameUIManager.cs
@@ -6,8 +6,8 @@ namespace BeatKeeper
     public class OutGameUIManager : MonoBehaviour
     {
         [SerializeField] private Image _pressAnyButtonImage;
-        [SerializeField] private AnimationCurve _fadeCurve;
-        [SerializeField] private float _fadeDuration = 1f;
+        [SerializeField, Tooltip("どのようにPressAnyButtonがフェードするかを設定")] private AnimationCurve _fadeCurve;
+        [SerializeField, Tooltip("PressAnyButtonのフェードが一周する時間を設定")] private float _fadeDuration = 1f; [SerializeField, Tooltip("ボタンを押した際に何秒かけて")] private float _onStartFadeIntensity = 0f;
 
         private void FixedUpdate()
         {
@@ -19,6 +19,14 @@ namespace BeatKeeper
                 color.a = alpha;
                 _pressAnyButtonImage.color = color;
             }
+        }
+
+        /// <summary>
+        /// スタートした際に呼び出されるメソッド。
+        /// </summary>
+        public void GameStart()
+        {
+
         }
     }
 }
