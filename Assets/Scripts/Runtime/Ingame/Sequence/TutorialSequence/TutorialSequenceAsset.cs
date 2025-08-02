9a733608014efd1cde0f9fc79234f4894cb69ecc
reitaghchi <ren.len2426@gmail.com>
2025-08-02 21:34:47 +0900

    [add]チュートリアルシークエンスのPlayableAssetクラス作成

---
+++ Assets/Scripts/Runtime/Ingame/Sequence/TutorialSequence/TutorialSequenceAsset.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace BeatKeeper.Runtime.Ingame.Sequence
+{
+    /// <summary>
+    /// チュートリアルシークエンスのPlayableAssetクラス
+    /// </summary>
+    public class TutorialSequenceAsset : PlayableAsset
+    {
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            
+        }
+    }
+}
