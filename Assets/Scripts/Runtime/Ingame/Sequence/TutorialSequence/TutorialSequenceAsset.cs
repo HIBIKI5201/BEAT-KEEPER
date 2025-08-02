57fa030c61500f2bfec7123f359112f5d0c60089
reitaghchi <ren.len2426@gmail.com>
2025-08-02 21:46:43 +0900

    [add]TutorialSequenceAssetの中身を作成

--- Assets/Scripts/Runtime/Ingame/Sequence/TutorialSequence/TutorialSequenceAsset.cs
+++ Assets/Scripts/Runtime/Ingame/Sequence/TutorialSequence/TutorialSequenceAsset.cs
@@ -10,7 +10,21 @@ namespace BeatKeeper.Runtime.Ingame.Sequence
     {
         public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
         {
-            
+            switch (_tutorialFase)
+            {
+                case 1:
+                    return SequenceBehaviourBase.CreatePlayable<TutorialSequenceBehaviour_1>(graph, owner);
+                case 2:
+                    return SequenceBehaviourBase.CreatePlayable<TutorialSequenceBehaviour_2>(graph, owner);
+                case 3:
+                    return SequenceBehaviourBase.CreatePlayable<TutorialSequenceBehaviour_3>(graph, owner);
+                case 4:
+                    return SequenceBehaviourBase.CreatePlayable<TutorialSequenceBehaviour_4>(graph, owner);
+                case 5:
+                    return SequenceBehaviourBase.CreatePlayable<TutorialSequenceBehaviour_5>(graph, owner);
+            }
         }
+
+        [SerializeField, Range(1, 3)] private int _tutorialFase;
     }
 }
