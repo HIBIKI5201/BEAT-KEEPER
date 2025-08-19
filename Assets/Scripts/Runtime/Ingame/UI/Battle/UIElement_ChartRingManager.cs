97aa9945303d83d666312224f45d6715dd42dea9
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-20 02:54:30 +0900

    [fix]不要な名前空間を削除

--- Assets/Scripts/Runtime/Ingame/UI/Battle/UIElement_ChartRingManager.cs
+++ Assets/Scripts/Runtime/Ingame/UI/Battle/UIElement_ChartRingManager.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Pool;
-using static UnityEditor.Rendering.FilterWindow;
 
 namespace BeatKeeper.Runtime.Ingame.UI
 {
