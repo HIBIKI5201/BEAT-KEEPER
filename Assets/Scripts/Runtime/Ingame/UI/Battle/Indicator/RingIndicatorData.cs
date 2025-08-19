a936dbc73fe3f75018025704b60d0211ce5e5ba3
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-20 03:58:56 +0900

    [update]エフェクトレングスを自動で取得するように

--- Assets/Scripts/Runtime/Ingame/UI/Battle/Indicator/RingIndicatorData.cs
+++ Assets/Scripts/Runtime/Ingame/UI/Battle/Indicator/RingIndicatorData.cs
@@ -1,7 +1,9 @@
 ﻿using BeatKeeper.Runtime.Ingame.Battle;
+using SymphonyFrameWork.Attribute;
 using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 namespace BeatKeeper.Runtime.Ingame.UI
 {
@@ -27,8 +29,48 @@ namespace BeatKeeper.Runtime.Ingame.UI
             return null;
         }
 
+        public bool TryGetRingData(ChartKindEnum attackKind, out RingData data)
+        {
+            foreach (var ringData in _ringDatas)
+            {
+                if (ringData.AttackKind == attackKind)
+                {
+                    data = ringData;
+                    return true;
+                }
+            }
+
+            Debug.LogWarning($"No RingData found for attack kind: {attackKind}");
+            data = null;
+            return false;
+        }
+
         [SerializeField, Tooltip("リングのデータ群")]
         private RingData[] _ringDatas;
+
+        private void OnValidate()
+        {
+            for (int i = 0; i < _ringDatas.Length; i++)
+            {
+                RingData data = _ringDatas[i];
+
+                if (data.RingPrefab == null)
+                {
+                    Debug.LogWarning($"RingPrefab is null for RingData at index {i}. Please assign a valid prefab.");
+                    continue;
+                }
+
+                if (data.RingPrefab.TryGetComponent(out RingIndicatorBase ring))
+                {
+                    _ringDatas[i] = new RingData(
+                        data.AttackKind,
+                        data.RingPrefab,
+                        data.DefaultCapacity,
+                        ring.EffectLength
+                    );
+                }
+            }
+        }
     }
 
     /// <summary>
@@ -37,6 +79,14 @@ namespace BeatKeeper.Runtime.Ingame.UI
     [Serializable]
     public class RingData
     {
+        public RingData(ChartKindEnum attackKind, GameObject ringPrefab, int defaultCapacity, int effectLength)
+        {
+            _attackKind = attackKind;
+            _ringPrefab = ringPrefab;
+            _defaultCapacity = defaultCapacity;
+            _effectLength = effectLength;
+        }
+
         public ChartKindEnum AttackKind => _attackKind;
         public GameObject RingPrefab => _ringPrefab;
         public int DefaultCapacity => _defaultCapacity;
@@ -50,7 +100,7 @@ namespace BeatKeeper.Runtime.Ingame.UI
         [Tooltip("リングの事前用意数（ある程度の同時出現数を入力）")]
         private int _defaultCapacity = 3;
 
-        [SerializeField]
+        [SerializeField, ReadOnly]
         private int _effectLength;
     }
 }
