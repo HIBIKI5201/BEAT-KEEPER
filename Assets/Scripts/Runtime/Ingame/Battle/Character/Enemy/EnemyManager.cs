5f7c1f296626ab6daa3ceb97ce7eccf4059335d7
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-20 02:11:33 +0900

    [fix]別データが混合しているのを仮にコミット

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Enemy/EnemyManager.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Enemy/EnemyManager.cs
@@ -1,5 +1,6 @@
 ﻿using BeatKeeper.Runtime.Ingame.Battle;
 using BeatKeeper.Runtime.Ingame.System;
+using BeatKeeper.Runtime.Ingame.UI;
 using BeatKeeper.Runtime.System;
 using Cysharp.Threading.Tasks;
 using R3;
@@ -42,6 +43,7 @@ namespace BeatKeeper.Runtime.Ingame.Character
             if (_bgmManager)
             {
                 _bgmManager.OnNearChangedBeat += OnAttack;
+                _bgmManager.OnJustChangedBeat += OnPrepareAttack;
             }
         }
 
@@ -53,6 +55,7 @@ namespace BeatKeeper.Runtime.Ingame.Character
             if (_bgmManager)
             {
                 _bgmManager.OnNearChangedBeat -= OnAttack;
+                _bgmManager.OnJustChangedBeat -= OnPrepareAttack;
             }
         }
 
@@ -129,12 +132,17 @@ namespace BeatKeeper.Runtime.Ingame.Character
         [SerializeField]
         private GameObject _normalAttackHitPerticle;
 
+        [SerializeField]
+        private RingIndicatorData _indicatorData;
+
         private BGMManager _bgmManager;
 
         private PlayerManager _target;
 
         private bool _canFinisher;
         private bool _isKnockback;
+
+        private int _normalAttackLength;
 
         private EnemyAnimeManager _animeManager;
         private CharacterHealthSystem _healthSystem;
@@ -147,7 +155,8 @@ namespace BeatKeeper.Runtime.Ingame.Character
 
         protected override void Awake()
         {
-            if (TryGetComponent(out Animator animator))
+            Animator animator = GetComponentInChildren<Animator>();
+            if (animator != null)
             {
                 _animeManager = new(animator);
             }
@@ -157,6 +166,11 @@ namespace BeatKeeper.Runtime.Ingame.Character
             }
 
             _healthSystem = new(_data);
+
+            _normalAttackLength = 
+                _indicatorData.GetRingData(ChartKindEnum.Normal).RingPrefab
+                    .GetComponent<RingIndicatorBase>()
+                    .EffectLength;
 
             SetActiveModel(false); //初期はモデル表示を無くす
         }
@@ -202,6 +216,7 @@ namespace BeatKeeper.Runtime.Ingame.Character
                 {
                     _target.HitAttack(new AttackData(1));
                     OnShootNormalAttack?.Invoke();
+                    _animeManager.Attack();
                 }
                 else if (attackKind == ChartKindEnum.Charge) //チャージアタック
                 {
@@ -214,6 +229,21 @@ namespace BeatKeeper.Runtime.Ingame.Character
                     _particleSystem?.Play();
                 }
             }
+        }
+
+        private void OnPrepareAttack()
+        {
+            if (_animeManager == null) return;
+
+            var timing = MusicEngineHelper.GetBeatSinceStart();
+
+            ChartKindEnum kind = _data.ChartData[timing + _normalAttackLength].AttackKind;
+
+            Debug.Log($"{timing + _normalAttackLength}t kind is {kind}");
+
+            if ((kind & ChartKindEnum.Normal) == 0) return; //ノーマルアタックでない場合は何もしない
+
+            _animeManager.PreAttack();
         }
 
         /// <summary>
