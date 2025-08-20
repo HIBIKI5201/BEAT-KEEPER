81bbfd8319aa837d8ef8a1faa7307d8c35c85670
reitaghchi <ren.len2426@gmail.com>
2025-08-20 19:59:14 +0900

    [add]タイムラインでボイスを流すためのスクリプト作成

---
+++ Assets/Scripts/Runtime/Ingame/Sequence/TutorialSequence/CriAtomSequenceBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using CriWare;
+using UnityEngine;
+using UnityEngine.Playables;
+using BeatKeeper.Runtime.System;
+namespace BeatKeeper
+{
+
+    public class CriAtomSequenceBehaviour : PlayableBehaviour
+    {
+        public string CueName;
+        public GameObject Owner;
+
+        public override void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            if (string.IsNullOrEmpty(CueName)) return;
+            VoiceManager.PlayVoice(CueName);
+        }
+    }
+
+}
