b0a3d05383f7103bd033f0e19a0ea78e7df2c291
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-23 13:25:16 +0900

    [update]不要なアニメーションパラメータを削除

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Enemy/EnemyAnimeManager.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Enemy/EnemyAnimeManager.cs
@@ -9,19 +9,17 @@ namespace BeatKeeper.Runtime.Ingame.Character
     {
         public EnemyAnimeManager(Animator animator) : base(animator) { }
 
-        public void KnockBack(int value = 0) => _animator?.SetTrigger(_knockBackHash);
+        public void KnockBack(bool value) => _animator?.SetBool(_knockBackHash, value);
         public void Attack() => _animator?.SetTrigger(_attackHash);
         public void PreAttack() => _animator?.SetTrigger(_preAttackHash);
 
-        public void ChargeAttackStart() => _animator?.SetTrigger(_chargeAttackHash);
-        public void ChargeAttackEnd() => _animator?.SetTrigger(_chargeAttackEndHash);
-        public void ChargeAttackCancel(bool value) => _animator?.SetBool(_chargeAttackCancel, value);
+        public void PreChargeAttack() => _animator?.SetTrigger(_chargeAttackHash);
+        public void ChargeAttack() => _animator?.SetTrigger(_chargeAttackEndHash);
 
         private readonly int _knockBackHash = Animator.StringToHash("KnockBack");
         private readonly int _attackHash = Animator.StringToHash("Attack");
         private readonly int _preAttackHash = Animator.StringToHash("PreAttack");
         private readonly int _chargeAttackHash = Animator.StringToHash("ChargeAttackStart");
         private readonly int _chargeAttackEndHash = Animator.StringToHash("ChargeAttackEnd");
-        private readonly int _chargeAttackCancel = Animator.StringToHash("ChargeAttackCancel");
     }
 }
