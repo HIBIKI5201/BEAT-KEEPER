907055bd1544863fab5ce786523abc4ae59baacd
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-08-25 00:19:31 +0900

    [fix]ランク関連のクラスをS~Cの設定だけになるように修正

--- Assets/Scripts/Runtime/System/Score/BattleGrade/BattleGradeEvaluator.cs
+++ Assets/Scripts/Runtime/System/Score/BattleGrade/BattleGradeEvaluator.cs
@@ -10,26 +10,12 @@ namespace BeatKeeper.Runtime.Ingame.System
     public class BattleGradeEvaluator : MonoBehaviour
     {
         /// <summary>
-        /// 今回のバトルで獲得したスコアを取得する
+        /// 引数に渡したスコアを元にバトルランクを計算する
         /// </summary>
-        public int CalculateBattleScore()
+        public BattleGradeEnum EvaluateRank(int battleScore)
         {
-            return _scoreManager.CalculateBattleScore();
-        }
-
-        /// <summary>
-        /// バトルランクを計算する
-        /// TODO: クリアフェーズで使用する
-        /// </summary>
-        public BattleGradeEnum EvaluateRank()
-        {
-            int battleScore = _scoreManager.CalculateBattleScore();
-            Debug.Log($"[BattleGradeEvaluator] バトルで獲得したスコア{battleScore}");
-
             return battleScore switch
             {
-                var score when score > _threshold.ThresholdRankSss => BattleGradeEnum.SSS,
-                var score when score > _threshold.ThresholdRankSs => BattleGradeEnum.SS,
                 var score when score > _threshold.ThresholdRankS => BattleGradeEnum.S,
                 var score when score > _threshold.ThresholdRankA => BattleGradeEnum.A,
                 var score when score > _threshold.ThresholdRankB => BattleGradeEnum.B,
@@ -38,11 +24,9 @@ namespace BeatKeeper.Runtime.Ingame.System
         }
 
         [SerializeField] private BattleGradeThresholdsSO _threshold;
-        private IScoreManager _scoreManager;
 
         private void Awake()
         {
-            _scoreManager = GetComponent<IScoreManager>();
             if (_threshold == null)
             {
                 Debug.LogError("[BattleGradeEvaluator] calculateRankDataSO が設定されていません！");
