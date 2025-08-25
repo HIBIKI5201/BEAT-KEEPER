b5c132ce8c774cfbb770c6f9409acee1bba33d99
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-08-25 11:18:54 +0900

    [add]コンボの賞賛ボイスを追加

--- Assets/Scripts/Runtime/Ingame/Battle/Character/Player/ComboSystem.cs
+++ Assets/Scripts/Runtime/Ingame/Battle/Character/Player/ComboSystem.cs
@@ -1,5 +1,7 @@
-﻿using R3;
+﻿using System.Collections.Generic;
+using R3;
 using UnityEngine;
+using BeatKeeper.Runtime.System;
 
 namespace BeatKeeper.Runtime.Ingame.Character
 {
@@ -18,7 +20,7 @@ namespace BeatKeeper.Runtime.Ingame.Character
         public void Attack()
         {
             _comboCount.Value++;
-            //_lastAttackTime = Time.time;
+            CheckAndPlayComboVoice(_comboCount.Value);
         }
 
         /// <summary>
@@ -33,17 +35,48 @@ namespace BeatKeeper.Runtime.Ingame.Character
         }
 
         // NOTE: 使用しなくなったが、互換性のためにメソッドは残してある
-        public void Update()
-        {
-            // //コンボ維持時間が終了するとリセット
-            // if (Time.time > _lastAttackTime + _data.ComboResetTime)
-            // {
-            //     ComboReset();
-            // }
-        }
+        public void Update() { }
 
+        // 各コンボボイスのcueName
+        private static readonly Dictionary<int, string[]> _comboVoiceMap = new()
+        {
+            { 10, new[] { "voice_combo_10_01", "voice_combo_10_02" } },
+            { 30, new[] { "voice_combo_30_01", "voice_combo_30_02" } },
+            { 50, new[] { "voice_combo_50_01", "voice_combo_50_02" } },
+            { 100, new[] { "voice_combo_100_01", "voice_combo_100_02" } }
+        };
+        private readonly string[] _highComboVoices = {"voice_combo_high_01", "voice_combo_high_02"};
+        
         private readonly PlayerData _data;
         private ReactiveProperty<int> _comboCount = new();
-        // private float _lastAttackTime = Time.time;
+
+        /// <summary>
+        /// コンボ数に応じてボイスを再生する
+        /// </summary>
+        private void CheckAndPlayComboVoice(int combo)
+        {
+            // 特定のコンボ数のボイスをチェック
+            if (_comboVoiceMap.TryGetValue(combo, out string[] voices))
+            {
+                PlayRandomVoice(voices);
+                return;
+            }
+
+            // 100コンボ以降で50の倍数の場合
+            if (combo > 100 && combo % 50 == 0)
+            {
+                PlayRandomVoice(_highComboVoices);
+            }
+        }
+        
+        /// <summary>
+        /// ボイスを再生する
+        /// </summary>
+        private void PlayRandomVoice(string[] voiceArray)
+        {
+            var rand = Random.Range(0, voiceArray.Length);
+            var cueName = voiceArray[rand];
+            VoiceManager.PlayVoice(cueName);
+        }
     }
 }
