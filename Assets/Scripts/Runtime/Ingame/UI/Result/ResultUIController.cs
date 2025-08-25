cfb460e28a94f541018ee79b649a58f21c301027
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-08-25 23:24:55 +0900

    [fix]一旦ボイス再生処理を追加

--- Assets/Scripts/Runtime/Ingame/UI/Result/ResultUIController.cs
+++ Assets/Scripts/Runtime/Ingame/UI/Result/ResultUIController.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.U2D;
+using System;
 using BeatKeeper.Runtime.Ingame.System;
+using BeatKeeper.Runtime.Ingame.Battle;
+using BeatKeeper.Runtime.System;
 
 namespace BeatKeeper
 {
@@ -16,9 +19,8 @@ namespace BeatKeeper
         /// <returns></returns>
         public void SetResult()
         {
-            SetScore();
-            SetRank();
-            SetRecords();
+            // 演出開始
+            StartPerformance();
         }
         
         [SerializeField] private ScoreManager _scoreManager; // スコアマネージャー
@@ -36,7 +38,15 @@ namespace BeatKeeper
         [SerializeField] private SpriteAtlas _rankSpriteAtlas; // ランクの画像のスプライトアトラス
         
         [SerializeField] private string _rankSpriteSuffix = "_rank"; // ランク画像のアセット名のサフィックス
+        
+        [Header("ボイスのCueNameの設定")]
+        [SerializeField] private string _resultCueName;
+        [SerializeField] private RankVoice[] _rankVoice = new RankVoice[4];
+        [SerializeField] private RankVoice[] _resultVoice = new RankVoice[4];
 
+        [Header("演出関連の設定")] 
+        [SerializeField, Tooltip("ランク発表までの待機時間")] private float _rankDuration = 3f;
+        
         /// <summary>
         /// Start
         /// </summary>
@@ -54,6 +64,35 @@ namespace BeatKeeper
             }
         }
 
+        /// <summary>
+        /// 演出を開始する
+        /// </summary>
+        private void StartPerformance()
+        {
+            ShowCanvas();
+            
+            // ボイス再生中にスコアのアニメーションと記録のアニメーションを再生
+            SetScore();
+            SetRecords();
+            
+            // ボイス再生とアニメーションが終わるまで待つ（_rankDuration）
+            SetRank();
+            
+            // ランク読み上げを待ってから賞賛ボイスを再生
+            PlayPraiseVoice();
+        }
+
+        /// <summary>
+        /// Canvasが有効になったときの処理
+        /// </summary>
+        private void ShowCanvas()
+        {
+            // NOTE: CanvasGroupの不透明度は現状ResultManagerから変更しているのでその処理は書いてない
+            
+            // ボイスを再生「今回のバトルレポートを確認するわ」
+            VoiceManager.PlayVoice(_resultCueName);
+        }
+        
         /// <summary>
         /// スコアの値を設定する
         /// </summary>
@@ -74,6 +113,10 @@ namespace BeatKeeper
             
             // スコアを元にランクを算出
             var rank = _gradeEvaluator.EvaluateRank(_scoreManager.Score);
+
+            // ランクに応じてボイス再生
+            var voice = GetRankVoiceCueName(rank, _rankVoice);
+            VoiceManager.PlayVoice(voice);
             
             // ランクの文字列とサフィックスを連結して、スプライトをロードしてくる
             var rankSprite = _rankSpriteAtlas.GetSprite($"{rank.ToString()}{_rankSpriteSuffix}");
@@ -89,10 +132,51 @@ namespace BeatKeeper
         /// </summary>
         private void SetRecords()
         {
+            // TODO: 演出をつける
             _maxCombo.SetAmount(_scoreManager.MaxCombo);
             _perfectCount.SetAmount(_scoreManager.AccuracyTracker.PerfectCount);
             _goodCount.SetAmount(_scoreManager.AccuracyTracker.GoodCount);
             _missCount.SetAmount(_scoreManager.AccuracyTracker.MissCount);
+        }
+
+        /// <summary>
+        /// 賞賛ボイスを再生
+        /// </summary>
+        private void PlayPraiseVoice()
+        {
+            // スコアを元にランクを算出
+            var rank = _gradeEvaluator.EvaluateRank(_scoreManager.Score);
+
+            // ランクに応じてボイス再生
+            var voice = GetRankVoiceCueName(rank, _resultVoice);
+            VoiceManager.PlayVoice(voice);
+        }
+        
+        /// <summary>
+        /// ランクボイスの配列から指定されたランクに対応するCueNameを取得する
+        /// </summary>
+        private string GetRankVoiceCueName(BattleGradeEnum rank, RankVoice[] voiceNames)
+        {
+            foreach (var rankVoice in voiceNames)
+            {
+                if (rankVoice.Rank == rank)
+                {
+                    return rankVoice.CueName;
+                }
+            }
+    
+            Debug.LogWarning($"{typeof(ResultUIController)}: ランク {rank} に対応するボイスが見つかりません");
+            return string.Empty;
+        }
+
+        [Serializable]
+        private struct RankVoice
+        {
+            [SerializeField] private BattleGradeEnum _rank;
+            [SerializeField] private string _cueName;
+            
+            public BattleGradeEnum Rank => _rank;
+            public string CueName => _cueName;
         }
     }
 }
