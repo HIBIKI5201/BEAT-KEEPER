22e6136aa1d6da9854a031d4a4d8eeb7ebe3ac7b
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-09-01 15:05:46 +0900

    feat:言語タイプの列挙型

---
+++ Assets/Scripts/Runtime/System/Localize/LanguageType.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace BeatKeeper
+{
+    /// <summary>
+    /// ローカライズ用　言語の種類
+    /// </summary>
+    public enum LanguageType
+    {
+        /// <summary>
+        /// 日本語
+        /// </summary>
+        Japanese,
+        
+        /// <summary>
+        /// 英語
+        /// </summary>
+        Englich,
+    }
+}
