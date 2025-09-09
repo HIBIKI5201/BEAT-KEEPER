bee592b6cdd9ff85bfc9f75ba22c194c276f1457
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-09-09 11:39:26 +0900

    [fix]ノーツの失敗演出を、PlayerManagerに依存して実行されるように変更

--- Assets/Scripts/Runtime/Ingame/UI/Battle/Indicator/RingIndicatorBase.cs
+++ Assets/Scripts/Runtime/Ingame/UI/Battle/Indicator/RingIndicatorBase.cs
@@ -156,6 +156,12 @@ namespace BeatKeeper.Runtime.Ingame.UI
         /// </summary>
         public virtual void PlayFailEffect()
         {
+            if (MusicEngineHelper.GetBeatNearerSinceStart() < _timing)
+            {
+                // ノーツのタイミングより前なら処理はスキップ
+                return;
+            }
+            
 			Unsubscribe();
 
             // Tweens配列をクリア
@@ -350,14 +356,13 @@ namespace BeatKeeper.Runtime.Ingame.UI
             var contractionSequence = DOTween.Sequence()
 
                 // Just判定まで縮小を行う
-                .Append(_ringImage.rectTransform.DOScale(_contractionScale, beatDuration * CONTRACTION_SPEED).SetEase(Ease.Linear))
-                
+                .Append(_ringImage.rectTransform.DOScale(_contractionScale, beatDuration * CONTRACTION_SPEED)
+                    .SetEase(Ease.Linear))
+
                 // Just判定を過ぎたら縮小は続行しつつ段々フェードアウトする
-                .Append(_ringImage.rectTransform.DOScale(_contractionScale * 0.5f, beatDuration * RECEPTION_TIME).SetEase(Ease.Linear))
-                .Join(CreateFadeSequence(beatDuration * RECEPTION_TIME))
-                
-                // シーケンスが中断されなかった場合はミス。失敗演出を行う
-                .OnComplete(() => PlayFailEffect());
+                .Append(_ringImage.rectTransform.DOScale(_contractionScale * 0.5f, beatDuration * RECEPTION_TIME)
+                    .SetEase(Ease.Linear))
+                .Join(CreateFadeSequence(beatDuration * RECEPTION_TIME));
             
             // Tweenを配列に保存
             if (_tweens != null && _tweens.Length > 1)
