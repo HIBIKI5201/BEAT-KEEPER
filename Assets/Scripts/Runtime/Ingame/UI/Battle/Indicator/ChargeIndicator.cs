51b0554bba9ff5af9c7511d9f2c69370980c72f3
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-09-16 16:34:09 +0900

    [fix]ノーツクラスの修正

--- Assets/Scripts/Runtime/Ingame/UI/Battle/Indicator/ChargeIndicator.cs
+++ Assets/Scripts/Runtime/Ingame/UI/Battle/Indicator/ChargeIndicator.cs
@@ -60,7 +60,10 @@ namespace BeatKeeper.Runtime.Ingame.UI
         
         private const float CONTRACTION_SPEED = 2; // 始点リングの収束にかける拍数
         private const float CHARGE_TIME = 2; // チャージにかかる拍
-        private const float RECEPTION_TIME = 0.45f; // Justタイミングのあとの判定受付時間 // TODO: PlayerDataから値をとってくるようにする
+        private const float RECEPTION_TIME = 0.5f; // Justタイミングのあとの判定受付時間
+        
+        [Header("判定調整用")]
+        [SerializeField, Range(0.9f, 1.0f)] private float _animationSpeed = 0.9f;
         
         [Header("コンポーネントの参照")] 
 		[SerializeField] private Image _startPositionRing; // 長押しの始めを示すリング
@@ -130,11 +133,11 @@ namespace BeatKeeper.Runtime.Ingame.UI
                 // .Join(_startPositionRing.DOColor(_chargeColor, totalDuration * 0.1f).SetEase(Ease.OutFlash)) // 自身
         
                 // メインのリング移動アニメーション（外側リングから内側リングへ）
-                .Append(_ringImage.rectTransform.DOScale(_centerRingsScale, totalDuration).SetEase(Ease.Linear))
+                .Append(_ringImage.rectTransform.DOScale(_centerRingsScale, totalDuration * _animationSpeed).SetEase(Ease.Linear))
                 
                 // 終点リングへ移動
-                .Join(_startPositionRing.rectTransform.DOMove(_endPositionRing.transform.position, totalDuration).SetEase(Ease.Linear))
-                .Join(_centerImage.rectTransform.DOMove(_endPositionRing.transform.position, totalDuration).SetEase(Ease.Linear))
+                .Join(_startPositionRing.rectTransform.DOMove(_endPositionRing.transform.position, totalDuration * _animationSpeed).SetEase(Ease.Linear))
+                .Join(_centerImage.rectTransform.DOMove(_endPositionRing.transform.position, totalDuration * _animationSpeed).SetEase(Ease.Linear))
 
                 .OnComplete(OnChargeComplete);
             
@@ -358,7 +361,7 @@ namespace BeatKeeper.Runtime.Ingame.UI
             _player.OnGoodChargeAttack += HandleGood; // Good成功
             
             // チャージ攻撃失敗
-            //_player.OnMissedCharging += PlayFailEffectCharging;
+            _player.OnMissedCharging += PlayFailEffectCharging;
             _player.OnMissChargeAttack += PlayFailEffectCharging;
         }
 		
@@ -368,7 +371,7 @@ namespace BeatKeeper.Runtime.Ingame.UI
             _player.OnGoodCharging -= OnPlayerCharge;
             _player.OnPerfectChargeAttack -= HandlePerfect;
             _player.OnGoodChargeAttack -= HandleGood;
-            //_player.OnMissedCharging -= PlayFailEffectCharging;
+            _player.OnMissedCharging -= PlayFailEffectCharging;
             _player.OnMissChargeAttack -= PlayFailEffectCharging;
         }
         
