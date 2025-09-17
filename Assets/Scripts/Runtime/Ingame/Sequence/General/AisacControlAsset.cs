795ffab02fb6301783169c31ca86900abd0f351e
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-18 01:35:56 +0900

    [add]Aisac変化作り途中

---
+++ Assets/Scripts/Runtime/Ingame/Sequence/General/AisacControlAsset.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace BeatKeeper
+{
+    public class AisacControlAsset : PlayableAsset
+    {
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            var playable = ScriptPlayable<AisacControlBehaviour>.Create(graph);
+            var behaviour = playable.GetBehaviour();
+            behaviour.SetData(_aisacName, _duration, _start, _end);
+
+            return playable;
+        }
+
+        [SerializeField]
+        private string _aisacName;
+        [SerializeField]
+        private float _duration = 1;
+
+        [SerializeField, Range(0, 1)]
+        private float _start;
+        [SerializeField, Range(0, 1)]
+        private float _end;
+
+    }
+}
---
+++ Assets/Scripts/Runtime/Ingame/Sequence/General/AisacControlBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using BeatKeeper.Runtime.Ingame.System;
+using CriWare;
+using SymphonyFrameWork.System;
+using SymphonyFrameWork.Utility;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace BeatKeeper
+{
+    public class AisacControlBehaviour : PlayableBehaviour
+    {
+        public void SetData(string aisac, float duration,float start, float end)
+        {
+            _aisac = aisac;
+            _duration = duration;
+            _start = start;
+            _end = end;
+        }
+
+        public override async void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            var bgmManager = ServiceLocator.GetInstance<BGMManager>();
+            await SymphonyTask.WaitUntil(() => bgmManager.AtomSource != null);
+
+            var player = bgmManager.AtomSource.player;
+
+            float timer = 0;
+            while (timer < _duration)
+            {
+                float proportion = timer / _duration;
+                player.SetAisacControl(_aisac, _start + _end * proportion);
+
+                timer += Time.deltaTime;
+                await Awaitable.NextFrameAsync();
+            }
+        }
+
+        private string _aisac;
+        private float _duration = 0;
+        private float _start;
+        private float _end;
+    }
+}
