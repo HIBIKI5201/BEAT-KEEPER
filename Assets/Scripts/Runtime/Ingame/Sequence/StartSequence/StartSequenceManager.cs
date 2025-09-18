a723bb1aa92bb0469dd44d24e845a372c20e0d66
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-09-18 10:50:46 +0900

    [fix]ベースクラスでまとめた部分を削除

--- Assets/Scripts/Runtime/Ingame/Sequence/StartSequence/StartSequenceManager.cs
+++ Assets/Scripts/Runtime/Ingame/Sequence/StartSequence/StartSequenceManager.cs
@@ -6,14 +6,13 @@ using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Playables;
 using Debug = UnityEngine.Debug;
-using UnityEngine.InputSystem;
 
 namespace BeatKeeper.Runtime.Ingame.Sequence
 {
     /// <summary>
     /// 開始演出
     /// </summary>
-    public class StartSequenceManager : MonoBehaviour
+    public class StartSequenceManager : BaseSkipController
     {
         [SerializeField] TutorialManager _tutorialManager;
         [SerializeField] PlayableAsset _playableAsset;
@@ -22,17 +21,10 @@ namespace BeatKeeper.Runtime.Ingame.Sequence
         [SerializeField] private float _skipTiming;
         
         [SerializeField] private string _bgmName = "Phase1";
-
-        [SerializeField, Tooltip("スキップする場合のホールド時間")] private float _holdDuration = 3f;
         
         private PlayableDirector _director;
         
-        private InputBuffer _inputBuffer;
-
-        private bool _isSkipping = false; // 現在スキップ入力中かどうか
-        private float _skipHoldTime = 0f; // 長押し時間の記録
-        
-        private async void Start()
+        protected override async void Start()
         {
             var multiSceneManager = ServiceLocator.GetInstance<MultiSceneManager>();
             if (multiSceneManager)
@@ -44,7 +36,6 @@ namespace BeatKeeper.Runtime.Ingame.Sequence
             var movieManager = await ServiceLocator.GetInstanceAsync<MovieManager>();
             var director = movieManager.GetDirector(_playableAsset);
             var phaseManager = await ServiceLocator.GetInstanceAsync<PhaseManager>();
-            _inputBuffer = await ServiceLocator.GetInstanceAsync<InputBuffer>();
             
             //スタートシーケンスの再生終了時にチュートリアルシーケンスを再生する
             director.stopped += (_) =>
@@ -60,42 +51,22 @@ namespace BeatKeeper.Runtime.Ingame.Sequence
 
             SaveDirector(director);
 
-            // スキップのインプットアクションを登録する
-            RegisterSkipInput();
+            base.Start();
         }
 
-        private void Update()
+        protected override void Update()
         {
             // タイムラインアセットがnullもしくはタイムラインの再生が終了していたらスキップ処理は行いたくないので早期return
             if (_director == null || _director.time >= _skipTiming)
             {
                 return;
             }
-            
-            if (_isSkipping)
-            {
-                // 時間の計測に経過時間を足していく
-                _skipHoldTime += Time.unscaledDeltaTime;
-                
-                if (_skipHoldTime >= _holdDuration)
-                {
-                    // 長押し時間がしきい値を越えたらスキップ
-                    // メソッドの呼び出しと変数のリセットを行う
-                    SkipStart();
-                    
-                    _isSkipping = false;
-                    _skipHoldTime = 0f;
-                }
-            }
-        }
 
-        private void OnDestroy()
-        {
-            UnregisterSkipInput();
+            base.Update();
         }
         
-        [ContextMenu(nameof(SkipStart))]
-        private void SkipStart()
+        [ContextMenu(nameof(OnSkip))]
+        protected override void OnSkip()
         {
             if (_director == null)
             {
@@ -115,48 +86,6 @@ namespace BeatKeeper.Runtime.Ingame.Sequence
             
             // タイムラインの時間をとばす
             _director.time = _skipTiming;
-        }
-        
-        /// <summary>
-        /// スキップのインプットアクションを登録する
-        /// </summary>
-        private void RegisterSkipInput()
-        {
-            if (_inputBuffer != null && _inputBuffer.Quit != null)
-            {
-                _inputBuffer.Quit.started += OnSkipStarted;
-                _inputBuffer.Quit.canceled += OnSkipCanceled;
-            }
-        }
-
-        /// <summary>
-        /// スキップのインプットアクション登録を解除する
-        /// </summary>
-        private void UnregisterSkipInput()
-        {
-            if (_inputBuffer != null && _inputBuffer.Quit != null)
-            {
-                _inputBuffer.Quit.started -= OnSkipStarted;
-                _inputBuffer.Quit.canceled -= OnSkipCanceled;
-            }
-        }
-        
-        /// <summary>
-        /// 長押しし始めの処理
-        /// </summary>
-        private void OnSkipStarted(InputAction.CallbackContext context)
-        {
-            _isSkipping = true;
-            _skipHoldTime = 0f;
-        }
-
-        /// <summary>
-        /// 長押しボタンを離した時の処理
-        /// </summary>
-        private void OnSkipCanceled(InputAction.CallbackContext context)
-        {
-            _isSkipping = false;
-            _skipHoldTime = 0f;
         }
         
         [Conditional("UNITY_EDITOR")]
