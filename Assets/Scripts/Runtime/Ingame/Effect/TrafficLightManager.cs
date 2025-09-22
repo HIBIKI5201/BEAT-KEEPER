4274baf116f20c5eed490151ff07974967570c93
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-22 23:54:41 +0900

    [add]信号のマネージャーを追加

---
+++ Assets/Scripts/Runtime/Ingame/Effect/TrafficLightManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace BeatKeeper
+{
+    [RequireComponent(typeof(Animator))]
+    public class TrafficLightManager : MonoBehaviour
+    {
+        public void ToPhase1()
+        {
+            _animator.SetBool(_phaseHash, false);
+        }
+
+        public void ToPhase3()
+        {
+            _animator.SetBool(_phaseHash, true);
+        }
+
+        private readonly int _phaseHash = Animator.StringToHash("Phase3");
+        
+        private Animator _animator;
+        private void Awake()
+        {
+            TryGetComponent(out _animator);
+        }
+    }
+}
