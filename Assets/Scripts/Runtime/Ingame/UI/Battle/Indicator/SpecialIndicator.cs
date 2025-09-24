54cc8ed8b8bfa1033a3a7e258608f4dc312fd487
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-09-24 15:37:10 +0900

    [fix]スキルノーツの中心UIが小さい問題

--- Assets/Scripts/Runtime/Ingame/UI/Battle/Indicator/SpecialIndicator.cs
+++ Assets/Scripts/Runtime/Ingame/UI/Battle/Indicator/SpecialIndicator.cs
@@ -267,7 +267,7 @@ namespace BeatKeeper.Runtime.Ingame.UI
                 _centerImage.sprite = _guideIcon;
                 
                 // 中央の操作イメージのサイズも変更する
-                _centerImage.rectTransform.sizeDelta *= _centerImageMultiply;
+                _centerImage.rectTransform.sizeDelta *= (_centerImageMultiply * _resolutionMultiply);
             }
             else
             {
@@ -277,7 +277,7 @@ namespace BeatKeeper.Runtime.Ingame.UI
                 _centerImage.sprite = _guide.Sprite;
                 
                 // 通常サイズ
-                _centerImage.rectTransform.sizeDelta = _guide.SizeDelta;
+                _centerImage.rectTransform.sizeDelta = _guide.SizeDelta * _resolutionMultiply;
             }
 
             _tweens[2] = sequence;
